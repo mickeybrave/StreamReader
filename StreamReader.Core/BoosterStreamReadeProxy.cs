9182c0aa62b15426cb0400851af537e942bc297d
mickeybrave <bravemick@gmail.com>
2024-05-24 19:25:29 +1200

    added conditrionals to precemt errors

--- StreamReader.Core/BoosterStreamReadeProxy.cs
+++ StreamReader.Core/BoosterStreamReadeProxy.cs
@@ -5,7 +5,7 @@
         ReaderResult<IStreamInfo> GetInfo(IStreamInfoCalculator streamInfoCalculator);
     }
 
-    public class BoosterStreamReaderProxy: IBoosterStreamReaderProxy
+    public class BoosterStreamReaderProxy : IBoosterStreamReaderProxy
     {
         private readonly ITextProcessor _textProcessor;
         private readonly string _text;
@@ -19,6 +19,28 @@
         {
             try
             {
+                if (streamInfoCalculator == null)
+                {
+                    return new ReaderResult<IStreamInfo>
+                    {
+                        Error = new Error
+                        {
+                            Message = "streamInfoCalculator cannot be empty"
+                        }
+                    };
+                }
+
+                if (string.IsNullOrEmpty(_text))
+                {
+                    return new ReaderResult<IStreamInfo>
+                    {
+                        Error = new Error
+                        {
+                            Message = "provided text cannot be empty"
+                        }
+                    };
+                }
+
                 return new ReaderResult<IStreamInfo>()
                 {
                     Result = streamInfoCalculator.GetStreamInfo(_text)
